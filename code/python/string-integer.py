2951a3866b0a9882ca7756fe73adb1c886977fc2
6abrielle <110362405+6abrielle@users.noreply.github.com>
2022-09-01 18:17:04 +1200

    Create string-integer.py

---
+++ code/python/string-integer.py
@@ -0,0 +1,5 @@
+# Write a Python program to check a whether a string represents an integer or not.
+
+# Your program should look as follows:
+  # Input a string: Python.
+  # The string is not an integer.
